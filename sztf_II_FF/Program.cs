d5897b746fb27679f9b9ddd9c32b8c5566dda0e5
Lam <longvulam91@gmail.com>
2019-03-27 14:01:01 +0100

    Init VS project

---
+++ sztf_II_FF/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sztf_II_FF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
